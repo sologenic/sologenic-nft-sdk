ce133eb4dcb3d92f7f8ecb6af47127dd8cc19502
Karamvir Singh <karamvir.singh98@gmail.com>
2022-12-31 10:12:14 -0800

    remove categories object

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
 export * from "./agent";
-export const categories = {
-  art: "art",
-  motion: "motion",
-  music: "music",
-  metaverse: "metaverse",
-  sports: "sports",
-  others: "others",
-  trading_cards: "tradingcards",
-  collectibles: "collectibles",
-};
